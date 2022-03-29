e1148921dde91eb01b0a8365cfbf033b95950e79
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-03-29 21:33:43 +0700

    feat(Proof-of-work): implement proof of work
    Basic features: nonce number, add nonce number to the hash function, add difficulty, add how to mine a block, mining block before adding to the blockchain

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
@@ -14,14 +15,26 @@ class Block {
       this.index +
         this.previousHash +
         this.timestamp +
-        JSON.stringify(this.data)
+        JSON.stringify(this.data) +
+        this.nonce
     ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log('block mined: ' + this.hash);
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
   }
 
   createGenesisBlock() {
@@ -33,7 +46,7 @@ class Blockchain {
   }
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
   isChainValid() {
@@ -52,3 +65,11 @@ class Blockchain {
     return true;
   }
 }
+
+// try to log the blockchain to console
+let myCoin = new Blockchain();
+console.log('mining coin 1...');
+myCoin.addBlock(new Block(1, '02/01/2000', { amount: 4 }));
+console.log('mining coin 2...');
+myCoin.addBlock(new Block(2, '03/01/2000', { amount: 42 }));
+console.log(myCoin);
