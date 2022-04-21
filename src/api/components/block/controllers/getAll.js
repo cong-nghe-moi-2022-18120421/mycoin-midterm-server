2a8c549df5ee48acc37df10181196a77f5ba9c3a
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2022-04-21 13:39:53 +0700

    refactor(getAll-blocks): reverse the chain in response

--- src/api/components/block/controllers/getAll.js
+++ src/api/components/block/controllers/getAll.js
@@ -1,7 +1,7 @@
 import myCoin from '../../../start/blockchain';
 
 const getAll = (req, res) => {
-  const chain = myCoin.chain;
+  const chain = myCoin.chain.reverse();
 
   res.status(200).send(chain);
 };
